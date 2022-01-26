b297e03dce91f4a0ecf3f48f907d1fd2befcff34
b3ik2 <boykoalex555@gmail.com>
2022-01-27 02:54:19 +0300

    first commit

---
+++ src/test/java/yandex/boyko/page/RegistrationPage.java
@@ -0,0 +1,114 @@
+package yandex.boyko.page;
+
+import com.codeborne.selenide.SelenideElement;
+import yandex.boyko.page.components.CalendarComponents;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationPage {
+
+  public CalendarComponents calendarComponent = new CalendarComponents();
+
+  SelenideElement
+          firstNameInput = $("#firstName"),
+          lastNameInput = $("#lastName"),
+          emailInput = $("#userEmail"),
+
+
+  phoneNumberInput = $("#userNumber"),
+          subjectsInput = $("#subjectsInput"),
+          checkResultForm = $(".table-responsive"),
+          uploadFile = $("input#uploadPicture"),
+          currentAdressInput = $("#currentAddress"),
+          selectState = $("#react-select-3-input"),
+          selectCity = $("#react-select-4-input"),
+          clickButton = $("#submit");
+
+
+  public RegistrationPage openPage() {
+    open("https://demoqa.com/automation-practice-form");
+
+    return this;
+  }
+
+  public RegistrationPage inputFirstName(String firstName) {
+    firstNameInput.setValue(firstName);
+
+    return this;
+  }
+
+  public RegistrationPage inputLastName(String lastName) {
+    lastNameInput.setValue(lastName);
+
+    return this;
+  }
+
+  public RegistrationPage inputEmeil(String email) {
+    emailInput.setValue(email);
+
+    return this;
+  }
+
+  public RegistrationPage putGender(String gender) {
+
+    $(byText(gender)).click();
+    return this;
+  }
+
+
+  public RegistrationPage inputPhoneNumber(String phoneNumber) {
+    phoneNumberInput.setValue(phoneNumber);
+
+    return this;
+  }
+
+  public RegistrationPage inputSubjects(String Subject) {
+    subjectsInput.setValue(Subject).pressEnter();
+
+    return this;
+  }
+
+  public RegistrationPage selectHobbies(String hobbies) {
+    $(byText(hobbies)).click();
+
+    return this;
+  }
+
+  public RegistrationPage uploadFileFromForm(String fileName) {
+    uploadFile.uploadFromClasspath(fileName);
+
+    return this;
+  }
+
+  public RegistrationPage inputCurrentAdress(String currentAddress) {
+    currentAdressInput.setValue(currentAddress);
+
+    return this;
+  }
+
+  public RegistrationPage selectStateInCheckbox(String selectState) {
+    this.selectState.setValue(selectState).pressEnter();
+
+    return this;
+  }
+
+  public RegistrationPage selectCityInCheckbox(String selectCity) {
+    this.selectCity.setValue(selectCity).pressEnter();
+
+    return this;
+  }
+
+  public RegistrationPage clickButtonSubmit() {
+    clickButton.click();
+    return this;
+  }
+
+  public RegistrationPage checkResultTable(String key, String value) {
+    checkResultForm.$(byText(key)).parent().shouldHave(text(value));
+
+    return this;
+  }
+}
---
+++ src/test/java/yandex/boyko/page/components/TestBase.java
@@ -0,0 +1,11 @@
+package yandex.boyko.page.components;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+  @BeforeAll
+  static void beforeAll() {
+    Configuration.browserSize = "1280x720";
+  }
+}
